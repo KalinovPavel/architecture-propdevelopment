e4b7e2cf97e802761c6e33be52bca49284f1a74f
KalinovPavel <kpm011@yandex.ru>
2025-03-31 11:20:02 +0300

    Update create-users.sh
    Добавлены новые комментарии.

--- Task4/create-users.sh
+++ Task4/create-users.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+# Применяем Roles и RoleBindings
 kubectl apply -f roles.yaml
 kubectl apply -f role-bindings.yaml
 
+# Создаём сервисные аккаунты (viewer, editor, admin) в namespace=default
 kubectl create serviceaccount viewer -n default
 kubectl create serviceaccount editor -n default
 kubectl create serviceaccount admin -n default
 
-# такой подход поддерживается в Kubernetes 1.24+
+# Генерируем (запрашиваем) их токены (доступно с k8s 1.24+)
 kubectl create token viewer -n default
 kubectl create token editor -n default
 kubectl create token admin -n default
